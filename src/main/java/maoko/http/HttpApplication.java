bce16968c1ca2fd70c03fea867e172ac4f2673aa
fanpei <fanpei1413@163.com>
2020-04-21 17:31:27 +0800

    去除内置tomcat

--- src/main/java/maoko/http/HttpApplication.java
+++ src/main/java/maoko/http/HttpApplication.java
@@ -1,5 +1,6 @@
 package maoko.http;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
@@ -7,21 +8,16 @@ import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 
+
+
 /**
- * 程序启动入口
- *
  * @author maoko
  */
-
 @PropertySource("classpath:application.yml")
-//默认加载数据库配置，当前无数据库配置，排除在外2019.03.16 fanpei
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class,
-        // gson转换排除
-        JacksonAutoConfiguration.class})
-public class HttpApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HttpApplication.class);
+@SpringBootApplication
+public class HttpApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("http client start.....");
     }
-
 }
