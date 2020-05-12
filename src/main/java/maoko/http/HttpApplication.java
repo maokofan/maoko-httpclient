75164ab38756e939fc57e244c188315896e0d351
fanpei <fanpei1413@163.com>
2020-05-12 23:37:54 +0800

    删除多余测试类； 删除多余pom依赖

--- src/main/java/maoko/http/HttpApplication.java
+++ src/main/java/maoko/http/HttpApplication.java
@@ -1,15 +1,5 @@
 package maoko.http;
 
-import maoko.sdk.SDKCommon;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
-
-
 /**
  * @author maoko
  */
